d096fef8e4d72dbd36f3ba75a221a6ad2e7ff497
Ellaylone <ellaylone@yandex-team.ru>
2017-01-08 11:59:32 +0300

    init 19

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,5 @@
+const video = document.querySelector('.player'),
+      canvas = document.querySelector('.photo'),
+      ctx = canvas.getContext('2d'),
+      strip = document.querySelector('.strip'),
+      snap = document.querySelector('.snap');
